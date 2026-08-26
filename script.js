755d52d50c90a4cf22d64627b7568183c69e778e
Keyue Yang <cathyyang0423@gmail.com>
2026-08-26 13:15:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 const navLinks = document.querySelectorAll(".nav-links a");
-
 const currentPage = window.location.pathname.split("/").pop() || "index.html";
 
 navLinks.forEach((link) => {
   const linkPage = link.getAttribute("href");
 
   if (linkPage === currentPage) {
-    link.classList.add("active");
-  }
-
-  if (currentPage === "" && linkPage === "index.html") {
     link.classList.add("active");
   }
 });
